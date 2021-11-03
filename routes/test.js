7187c6cc180de95be8fa0117d3a458950318403d
RomainM81 <dcromain112@gmail.com>
2021-11-03 16:04:37 +0000

    test adding params from front on test.js

--- routes/test.js
+++ routes/test.js
@@ -6,27 +6,31 @@ const axios = require('axios')
 const router = express.Router()
 
 const cookie = [
-  '.ASPXAUTH=DD495BCF89896AFA49F5C0B8F69442ECAAC0D325E313DAB5987C9F5372C31959957C34F302271B9171C253F48DCE61ABC1FF576B9FBA667485311C16791A1B60B9C4F9A115A797B697CEFC11E6737D0B2D823D10BE723161D57B95F36B0AC6B054E1F97458946EBF3B921903180AEEA28A7FD5A0; ASP.NET_SessionId=gdaxhsfvliubeikxzsckumf4'
+  '.ASPXAUTH=DA72FF33252043E7E8766786F4C892C2F20532A3C8DC30CF57CEB7EA330A915298395DC9E5C24D68051A882E224AE1365C7EF5C8DF3B0F662718F6D0DBF2A34C0AF2F790C005E1A1468B0D83722D4B7891B2472C2BA030337B45AD5673B60CA5349C06DB88BCDDFB4AE57B9DC13F4D80A02B02CF; ASP.NET_SessionId=gbvwczwmatfkd325p34ghnbw'
 ]
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
   const player = []
+  const playerId = req.params.id
   const getApiData = async () => {
     try {
-      const getPlayer = await axios
-        .get(`https://bbapi.buzzerbeater.com/player.aspx?playerid=50019036`, {
+      const getPlayer = await axios.get(
+        `https://bbapi.buzzerbeater.com/player.aspx?playerid=${playerId}`,
+
+        {
           headers: {
             Cookie: cookie[0]
           }
-        })
-        .then(response => {
-          // console.log('player : ', response)
-          parseString(response.data, function (err, result) {
-            player.push(result.bbapi.player[0])
-            console.log(player)
-            res.status(200).send(player)
-          })
-        })
+        }
+      )
+      // .then(response => {
+      // console.log('player : ', getPlayer.data)
+      parseString(getPlayer.data, function (err, result) {
+        player.push(result.bbapi.player[0])
+        console.log('P', player)
+        res.status(200).send(player)
+      })
+      // })
     } catch (error) {
       console.error(error)
     }
